ba69c63689a21d1c663310802b1752103d59e3c1
VesnaPivac <33427475+VesnaPivac@users.noreply.github.com>
2021-12-08 20:26:04 -0700

    Update entrenador.py

--- entrenador.py
+++ entrenador.py
@@ -1,11 +1,11 @@
 import nltk
+import json
+import pickle
+import numpy as np
 nltk.download('punkt')
 nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
-import json
-import pickle
-import numpy as np
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.optimizers import SGD
